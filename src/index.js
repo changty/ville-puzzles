d053377530ef8650de713b4e33b0e9612b851886
Lauri Junkkari <latenssi@gmail.com>
2018-04-21 20:59:22 +0300

    Minor adjustment to init

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ import initPuzzle3 from "./initPuzzle3";
 
 // initPuzzle3();
 
+function activatePuzzle(init) {
+  if (window.activePuzzle && window.activePuzzle.cleanUp)
+    window.activePuzzle.cleanUp();
+  window.activePuzzle = init();
+}
+
 document.querySelector("#initPuzzle1").onclick = () => {
-  if (window.puzzle1 && window.puzzle1.cleanUp) window.puzzle1.cleanUp();
-  window.puzzle1 = initPuzzle1();
+  activatePuzzle(initPuzzle1);
 };
 
 document.querySelector("#initPuzzle2").onclick = () => {
-  if (window.puzzle2 && window.puzzle2.cleanUp) window.puzzle2.cleanUp();
-  window.puzzle2 = initPuzzle2();
+  activatePuzzle(initPuzzle2);
 };
 
 document.querySelector("#initPuzzle3").onclick = () => {
-  if (window.puzzle3 && window.puzzle3.cleanUp) window.puzzle3.cleanUp();
-  window.puzzle3 = initPuzzle3();
+  activatePuzzle(initPuzzle3);
 };
